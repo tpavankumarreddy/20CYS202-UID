03f3327b83dab9a4fcfcab8148314b9b1a51425b
tpavankumarreddy <92919687+tpavankumarreddy@users.noreply.github.com>
2023-01-07 20:03:36 +0530

    Add files via upload

---
+++ Mini-Project/js/login_2.js
@@ -0,0 +1,34 @@
+function auth() {
+
+    const username = login.id.value;
+    const password = login.pass.value;
+
+    if (username === "admin" && password ==="root") {
+      alert("You have successfully logged in.");           
+      window.open('html/admin.html');
+    }
+
+    else if (username === "student" && password ==="password") {
+      alert("You have successfully logged in."); 
+      window.open("html/studash.html");
+      return false;
+    }
+
+    else if (username === "faculty" && password ==="123abc") {
+      alert("You have successfully logged in."); 
+      window.open("html/faculty.html");
+      return false;
+    }
+
+    else if (username === "hod" && password ==="123456789") {
+      alert("You have successfully logged in."); 
+      window.open("html/hod.html");
+      return false;
+    }
+
+    else {
+      alert("Enter correct credentials");
+      return true;
+    }
+  
+  }
